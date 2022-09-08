8ce75a1ae6a0ad78b1bdc6669e1c8ceb3325c3f9
Camilo Velez <camilovelezfactored@FACTORED-CXV9V4R92N.local>
2022-09-08 14:22:34 -0500

    added a df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title("My parents healthy menu")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+food_df = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+streamlit.dataframe(food_df)
