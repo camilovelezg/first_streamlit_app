5265028f98ffbe17136c7d3e9508babc3b855141
Camilo Velez <camilovelezfactored@FACTORED-CXV9V4R92N.local>
2022-09-10 19:59:29 -0500

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,16 @@ def get_snowflake_data(connection):
 def add_fruits(connection, new_fruits):
     streamlit.text(f"Thank you for adding {new_fruits}")
     with connection.cursor() as cursor:
-        cursor.execute("insert into fruit_load_list (fruit_name) values (%(new_fruit)s)", {'new_fruit': new_fruits})
-        # new_fruits = new_fruits.split(",")
-        # query = ''
-        # for fruit in new_fruits:
-        #     query += f"({fruit.strip()}),"
-        # query = query[:-1]
-        # streamlit.text(query)
-        # cursor.execute("insert into fruit_load_list (fruit_name) values %(query)s", {'query': query})
+        # cursor.execute("insert into fruit_load_list (fruit_name) values (%(new_fruit)s)", {'new_fruit': new_fruits})
+        new_fruits = new_fruits.split(",")
+        query = ''
+        for fruit in new_fruits:
+            query += f"({fruit.strip()}),"
+        query = query[:-1]
+        streamlit.text(f"insert into fruit_load_list (fruit_name) values {query}")
+        streamlit.text(query)
+        cursor.execute("insert into fruit_load_list (fruit_name) values %(query)s", {'query': query})
+        streamlit.text(cursor.query.decode('utf-8'))
 
 
 streamlit.title("My parents healthy menu")
