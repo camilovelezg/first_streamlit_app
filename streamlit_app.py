3e999a0ff7e03d74523fd63f2a7d7755883642c7
Camilo Velez <camilovelezfactored@FACTORED-CXV9V4R92N.local>
2022-09-10 19:00:44 -0500

    closed the connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,19 @@ from urllib.error import URLError
 import streamlit
 import snowflake.connector
 
-snowflake_cursor = snowflake.connector.connect(**streamlit.secrets["snowflake"]).cursor()
-
 def get_fruityvice_df(selected_fruit):
     fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{selected_fruit}")
     return pandas.json_normalize(fruityvice_response.json())
 
-def get_snowflake_data(cursor):
-    return cursor.execute("select * from fruit_load_list").fetchall()
+def get_snowflake_data(connection):
+    streamlit.header("The fruit list contains:")
+    with connection.cursor() as cursor:
+        return cursor.execute("select * from fruit_load_list").fetchall()
 
-def add_fruit(cursor, new_fruit):
-    cursor.execute("insert into fruit_load_list (fruit_name) values(%(new_fruit)s)", {'new_fruit': new_fruit})
+def add_fruit(connection, new_fruit):
+    streamlit.text(f"Thank you for adding {new_fruit}")
+    with connection.cursor() as cursor:
+        cursor.execute("insert into fruit_load_list (fruit_name) values(%(new_fruit)s)", {'new_fruit': new_fruit})
 
 streamlit.title("My parents healthy menu")
 streamlit.header('Breakfast Menu')
@@ -25,13 +27,14 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 new_fruit = streamlit.text_input("Which fruit would you like to add?", "")
 if streamlit.button("Add fruit"):
-    streamlit.text(f"Thank you for adding {new_fruit}")
-    add_fruit(snowflake_cursor, new_fruit)
+    snowflake_connection = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    add_fruit(snowflake_connection, new_fruit)
+    snowflake_connection.close()
 
 if streamlit.button("Get fruit list"):
-    streamlit.header("The fruit list contains:")
-    my_snowflake_data = get_snowflake_data(snowflake_cursor)
-    streamlit.dataframe(my_snowflake_data)
+    snowflake_connection = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.dataframe(get_snowflake_data(snowflake_connection))
+    snowflake_connection.close()
 
 fruits_df = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruits_df = fruits_df.set_index("Fruit")
