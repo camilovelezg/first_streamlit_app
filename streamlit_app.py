3fe26baca7967a2d7bf33fd90b11a7a26ec6ebef
Camilo Velez <camilovelezfactored@FACTORED-CXV9V4R92N.local>
2022-09-10 19:45:47 -0500

    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,14 @@ def get_snowflake_data(connection):
 def add_fruits(connection, new_fruits):
     streamlit.text(f"Thank you for adding {new_fruits}")
     with connection.cursor() as cursor:
-        new_fruits = new_fruits.split(",")
-        query = ''
-        for fruit in new_fruits:
-            query += f"({fruit.strip()}),"
-        query = query[:-1]
-        streamlit.text(query)
-        cursor.execute("insert into fruit_load_list (fruit_name) values %(query)s", {'query': query})
+        cursor.execute("insert into fruit_load_list (fruit_name) values %(new_fruit)s", {'new_fruit': new_fruits})
+        # new_fruits = new_fruits.split(",")
+        # query = ''
+        # for fruit in new_fruits:
+        #     query += f"({fruit.strip()}),"
+        # query = query[:-1]
+        # streamlit.text(query)
+        # cursor.execute("insert into fruit_load_list (fruit_name) values %(query)s", {'query': query})
 
 
 streamlit.title("My parents healthy menu")
